eee6697f9aad62e211327565f97012afc06f57b6
Tom Groechel <tgroeche@umich.edu>
2023-04-25 13:56:13 -0500

    Back to linux

--- src/defer_node.cpp
+++ src/defer_node.cpp
@@ -4,6 +4,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include "lifecycle_prac/deferrable_callback_waitable.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
 using namespace std::chrono_literals;
 
@@ -11,7 +12,7 @@ class DeferNode : public rclcpp::Node
 {
 public:
   DeferNode()
-      : Node("minimal_param_node"), dcw_()
+      : Node("defer_node"), dcw_()
   {
     auto callback = std::make_shared<DeferrableCallbackWrapper<void>>(
         std::bind(&DeferNode::print_data, this, std::placeholders::_1));
@@ -23,6 +24,12 @@ public:
     this->get_node_waitables_interface()
         ->add_waitable(
             dcw_, (rclcpp::CallbackGroup::SharedPtr) nullptr);
+    // initialize timer
+    timer_ = this->create_wall_timer(
+        1s, std::bind(&DeferNode::timer_callback, this));
+
+    // create a client
+    client_ = this->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
   }
 
   void print_data(int data)
@@ -30,9 +37,41 @@ public:
     std::cout << "response: " << data << std::endl;
   }
 
+  void timer_callback()
+  {
+    if (deferal_sent)
+    {
+      return;
+    }
+    deferal_sent = true;
+    using ServiceResponseFuture =
+        rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedFutureWithRequest;
+    auto response_received_callback =
+        [logger = this->get_logger(), dcw_ = this->dcw_, deferal_sent = this->deferal_sent](ServiceResponseFuture future)
+    {
+      auto request_response_pair = future.get();
+      RCLCPP_INFO(
+          logger,
+          "Result of %" PRId64 " + %" PRId64 " is: %" PRId64,
+          request_response_pair.first->a,
+          request_response_pair.first->b,
+          request_response_pair.second->sum);
+      dcw_->send_resp(request_response_pair.second->sum);
+    };
+    auto result = client_->async_send_request(
+        request, std::move(response_received_callback));
+    RCLCPP_INFO(
+        this->get_logger(),
+        "Sending a request to the server (request_id =%" PRId64
+        "), we're going to let you know the result when ready!",
+        result.request_id);
+  }
+
 private:
+  bool deferal_sent{false};
   rclcpp::TimerBase::SharedPtr timer_;
   std::shared_ptr<DeferrableCallbackWaitable> dcw_;
+  // create an async client
 };
 
 int main(int argc, char **argv)
---
+++ src/simple_two_ints.cpp
@@ -0,0 +1,32 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+#include <memory>
+#include <chrono>
+
+void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+         std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+{
+    response->sum = request->a + request->b;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld"
+                                              " b: %ld",
+                request->a, request->b);
+    // sleep for 2 seconds to simulate a long service call
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
+        node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+}
