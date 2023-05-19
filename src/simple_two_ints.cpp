f8e8b40d9c1f20cca9a5b805f050c19f322c9c11
Thomas Groechel <tgroechel@irobot.com>
2023-05-19 08:59:04 -0500

    double srv resp

--- src/simple_two_ints.cpp
+++ src/simple_two_ints.cpp
@@ -4,16 +4,37 @@
 #include <memory>
 #include <chrono>
 
-void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-         std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+void add(
+    const std::shared_ptr<rclcpp::Service<example_interfaces::srv::AddTwoInts>> service_handle,
+    const std::shared_ptr<rmw_request_id_t> header,
+    const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request)
 {
-    response->sum = request->a + request->b;
+    // response->sum = request->a + request->b;
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld"
                                               " b: %ld",
                 request->a, request->b);
-    // sleep for 2 seconds to simulate a long service call
+    // send first response
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sending first response then sleeping");
+    example_interfaces::srv::AddTwoInts::Response resp;
+    resp.sum = 1;
+    service_handle->send_response(*header, resp);
+    // sleep
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sending second response then ending");
+
+    // send second response
+    example_interfaces::srv::AddTwoInts::Response resp2;
+    resp.sum = 2;
+    service_handle->send_response(*header, resp2);
+    service_handle->send_response(*header, resp2);
+    service_handle->send_response(*header, resp2);
+    service_handle->send_response(*header, resp2);
+
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "done");
+
+
+    // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+
 }
 
 int main(int argc, char **argv)
